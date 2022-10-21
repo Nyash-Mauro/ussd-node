f9bb630f473eb3d496a3c29d4decd993d98cd511
Nyash-Mauro <mauricee423@gmail.com>
2022-10-22 00:22:54 +0300

    created a boilerplate for the ussd

--- index.js
+++ index.js
@@ -1,40 +1,55 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.post('/ussd', (req, res) => {
-    // Read the variables sent via POST from our API
-    const {
-        phoneNumber,
-        text,
-    } = req.body;
-
-    let response = '';
-//lambda
-    if (text == '') {
-        // This is the first request. Note how we start the response with CON
-        response = `CON Welcome to Zero-taka-tech
-        1. New User
-        2. `;
-    } else if ( text == '1') {
-        // Business logic for first level response
-        response = `CON Choose account information you want to view
-        1. Select`;
-    } else if ( text == '2') {
-        // Business logic for first level response
-        // This is a terminal request. Note how we start the response with END
-        response = `END Your phone number is ${phoneNumber}`;
-    } else if ( text == '1*1') {
-        // This is a second level response where the user selected 1 in the first instance
-        const accountNumber = 'ACC100101';
-        // This is a terminal request. Note how we start the response with END
-        response = `END Your account number is ${accountNumber}`;
+const UssdMenu = require('ussd-menu-builder');
+// https://www.npmjs.com/package/ussd-menu-builder
+exports.handler = (event, context, callback) => {
+    
+    const menu = new UssdMenu();
+let args = {
+        phoneNumber: event.phoneNumber,
+        sessionId: event.sessionId,
+        serviceCode: event.serviceCode,
+        text: event.text
+    };
+// Define menu states
+    let startState = {
+        run: () => {
+            // use menu.con() to send response without terminating session      
+            menu.con('Welcome to Zero-taka-tech' + 
+                '\n--------------' +
+                '\n1. First Menu Option' +
+                '\n0. Exit');
+        },
+        // next object links to next state based on user input
+        next: {
+            '1': 'first-option',
+            '0': 'exit'
+        }
     }
-
-    // Send the response back to the API
-    res.set('Content-Type: text/plain');
-    res.send(response);
+    
+    menu.startState(startState);
+    menu.state('start', startState);
+    
+    menu.state('first-option', {
+        run: () => {
+            // use menu.con() to send response without terminating session      
+            menu.con('This is the first option' + 
+                '\n--------------' +
+                '\n9. Back' +
+                '\n0. Exit');
+        },
+        // next object links to next state based on user input
+        next: {
+            '9': 'start',
+            '0': 'exit'
+        }
 });
+    
+    menu.state('exit', {
+        run: () => {
+            menu.end('Thank you. Goodbye.');
+        }
+    });
+menu.run(args, resMsg => {
+        console.log(resMsg)
+        callback(null, resMsg);
+    });
+};
